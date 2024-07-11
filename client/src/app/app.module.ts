74c71da7fd16769d445f7846c1baf4e71b37b3ab
Thakur <E2020625@nventco.com>
2024-07-11 13:22:52 +0530

    implemented mat-icons

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSelectModule } from '@angular/material/select';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
 import { MatDialogModule } from '@angular/material/dialog';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BuyComponent } from './my-components/buy/buy.component';
+import { CustomerComponent } from './my-components/customer/customer.component';
+import { FormComponent } from './my-components/form/form.component';
+import { InvalidComponent } from './my-components/invalid/invalid.component';
 import { LoginComponent } from './my-components/login/login.component';
 import { NavbarComponent } from './my-components/navbar/navbar.component';
 import { SellerComponent } from './my-components/seller/seller.component';
-import { CustomerComponent } from './my-components/customer/customer.component';
-import { TableComponent } from './my-components/table/table.component';
-import { FormComponent } from './my-components/form/form.component';
-import { DataService } from './services/data.service';
-import { BuyComponent } from './my-components/buy/buy.component';
 import { SureComponent } from './my-components/sure/sure.component';
-import { InvalidComponent } from './my-components/invalid/invalid.component';
+import { TableComponent } from './my-components/table/table.component';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -54,6 +55,7 @@ import { InvalidComponent } from './my-components/invalid/invalid.component';
     MatTableModule,
     MatDialogModule,
     ReactiveFormsModule,
+    MatIconModule,
   ],
   providers: [DataService],
   bootstrap: [AppComponent],
